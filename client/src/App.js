dd919a3327c7e309f94b4308871b4d9d9e808480
Júlio Armando <julioarmando321@gmail.com>
2020-09-14 18:47:36 -0300

    Removendo estrutura e transformando em components

--- client/src/App.js
+++ client/src/App.js
@@ -1,99 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
+import M from 'materialize-css';
 
 import './app.css';
 
 import api from './Services/TransactionService';
-import Transaction from './Components/Transaction';
-import NewTransactionModal from './Components/NewTransactionModal';
+import ListAllMonth from './Utils/AllMonth';
+
+import PreLoader from './Components/Loader/PreLoader';
+import ListTransaction from './Components/Transactions/ListTransaction';
+import NewTransactionModal from './Components/Modal/NewTransactionModal';
+import StatsBar from './Components/Bars/StatsBar';
+import SearchBar from './Components/Bars/SearchBar';
 
 export default function App() {
-	const [transactions, setTransactions] = useState([]);
+	const [transactions, setTransactions] = useState();
 	const [period, setPeriod] = useState(moment().format('YYYY-MM'));
-	const [allMonth] = useState([
-		{ yearMonth: '2019-01', month: '01', year: '2019' },
-		{ yearMonth: '2019-02', month: '02', year: '2019' },
-		{ yearMonth: '2019-03', month: '03', year: '2019' },
-		{ yearMonth: '2019-04', month: '04', year: '2019' },
-		{ yearMonth: '2019-05', month: '05', year: '2019' },
-		{ yearMonth: '2019-06', month: '06', year: '2019' },
-		{ yearMonth: '2019-07', month: '07', year: '2019' },
-		{ yearMonth: '2019-08', month: '08', year: '2019' },
-		{ yearMonth: '2019-09', month: '09', year: '2019' },
-		{ yearMonth: '2019-10', month: '10', year: '2019' },
-		{ yearMonth: '2019-11', month: '11', year: '2019' },
-		{ yearMonth: '2019-12', month: '12', year: '2019' },
-		{ yearMonth: '2020-01', month: '01', year: '2020' },
-		{ yearMonth: '2020-02', month: '02', year: '2020' },
-		{ yearMonth: '2020-03', month: '03', year: '2020' },
-		{ yearMonth: '2020-04', month: '04', year: '2020' },
-		{ yearMonth: '2020-05', month: '05', year: '2020' },
-		{ yearMonth: '2020-06', month: '06', year: '2020' },
-		{ yearMonth: '2020-07', month: '07', year: '2020' },
-		{ yearMonth: '2020-08', month: '08', year: '2020' },
-		{ yearMonth: '2020-09', month: '09', year: '2020' },
-		{ yearMonth: '2020-10', month: '10', year: '2020' },
-		{ yearMonth: '2020-11', month: '11', year: '2020' },
-		{ yearMonth: '2020-12', month: '12', year: '2020' },
-		{ yearMonth: '2021-01', month: '01', year: '2021' },
-		{ yearMonth: '2021-02', month: '02', year: '2021' },
-		{ yearMonth: '2021-03', month: '03', year: '2021' },
-		{ yearMonth: '2021-04', month: '04', year: '2021' },
-		{ yearMonth: '2021-05', month: '05', year: '2021' },
-		{ yearMonth: '2021-06', month: '06', year: '2021' },
-		{ yearMonth: '2021-07', month: '07', year: '2021' },
-		{ yearMonth: '2021-08', month: '08', year: '2021' },
-		{ yearMonth: '2021-09', month: '09', year: '2021' },
-		{ yearMonth: '2021-10', month: '10', year: '2021' },
-		{ yearMonth: '2021-11', month: '11', year: '2021' },
-		{ yearMonth: '2021-12', month: '12', year: '2021' },
-	]);
-	const [receita, setReceita] = useState(0);
-	const [despesa, setDespesa] = useState(0);
+	const [allMonth, setAllMonth] = useState();
 	const [modalIsOpen, setIsOpen] = useState(false);
+	const [messageUser, setMessageUser] = useState();
+	const [filterText, setFilterText] = useState();
+
+	useEffect(() => {
+		setAllMonth(ListAllMonth);
+	}, [allMonth]);
 
 	useEffect(() => {
 		const auto = async () => {
+			setTransactions();
 			const response = await api.get(period);
+
 			setTransactions(response.data);
 			console.log(response.data);
-			countDespesas(response);
-			countReceitas(response);
+			M.AutoInit();
 		};
 		auto();
-	}, [period, modalIsOpen]);
-
-	const countDespesas = response => {
-		const despesa = response.data.reduce((acc, { value, type }) => {
-			if (type === '-') {
-				return acc + value;
-			}
-			return acc;
-		}, 0);
-		setDespesa(despesa);
-	};
-	const countReceitas = response => {
-		const receita = response.data.reduce((acc, { value, type }) => {
-			if (type === '+') {
-				return acc + value;
-			}
-			return acc;
-		}, 0);
-		setReceita(receita);
-	};
+	}, [period, modalIsOpen, messageUser]);
 
 	const changeMonth = event => {
 		setPeriod(event.target.value);
+	};
+
+	const nextMonth = () => {
+		console.log(allMonth.indexOf(period));
+		// setPeriod()
 	};
 
 	const clickNovoLancamento = () => {
 		setIsOpen(true);
 	};
 
+	const handleMessageUser = message => {
+		setMessageUser(message);
+	};
+
+	const changeFilterSearch = critery => {
+		console.log(critery.toLowerCase());
+		const listFiltered = transactions.filter(({ description }) => {
+			return new RegExp(`${critery}`, 'igu').test(
+				description.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+			);
+		});
+
+		listFiltered.lenght === 0
+			? setFilterText()
+			: setFilterText(listFiltered);
+		!critery ? setFilterText() : setFilterText(listFiltered);
+	};
+
 	const closeModal = () => {
 		setIsOpen(false);
 	};
-
 	return (
 		<div className="container">
 			<div className="center">
@@ -130,72 +107,40 @@ export default function App() {
 							);
 						})}
 				</select>
-				<button className="waves-effect waves-light btn">
+				<button
+					className="waves-effect waves-light btn"
+					onClick={nextMonth}
+				>
 					{' > '}
 				</button>
 			</div>
-			<div className="stats-bar">
-				<span>Lançamentos: {transactions.length}</span>
-				<span>
-					Receitas:{' '}
-					{receita.toLocaleString('pt-br', {
-						style: 'currency',
-						currency: 'BRL',
-						minimumFractionDigits: 2,
-					})}
-				</span>
-				<span>
-					Despesas:{' '}
-					{despesa.toLocaleString('pt-br', {
-						style: 'currency',
-						currency: 'BRL',
-						minimumFractionDigits: 2,
-					})}
-				</span>
-				<span>
-					Saldo:{' '}
-					{(receita - despesa).toLocaleString('pt-br', {
-						style: 'currency',
-						currency: 'BRL',
-						minimumFractionDigits: 2,
-					})}
-				</span>
-			</div>
+			{filterText ? (
+				<StatsBar transactions={filterText} /> // passar novo array e fazer ele ser contado no componente statsBar
+			) : (
+				transactions && <StatsBar transactions={transactions} />
+			)}
 
-			<div className="search-bar">
-				<button
-					className="waves-effect waves-light btn"
-					onClick={clickNovoLancamento}
-				>
-					+ Novo lançamento
-				</button>
-				<div className="input-field search">
-					<input type="text" placeholder="Filtro" />
-				</div>
-			</div>
+			<SearchBar
+				clickNovoLancamento={clickNovoLancamento}
+				searchCritery={changeFilterSearch}
+				visibleBar={setIsOpen}
+			/>
+
 			<div className="center">
-				{transactions.map((transaction, idx) => {
-					return (
-						<Transaction
-							key={idx}
-							day={transaction.day.toLocaleString('pt-br', {
-								minimumIntegerDigits: 2,
-							})}
-							category={transaction.category}
-							description={transaction.description}
-							value={transaction.value.toLocaleString('pt-br', {
-								style: 'currency',
-								currency: 'BRL',
-								minimumFractionDigits: 2,
-							})}
-							color={
-								transaction.type === '+'
-									? 'rgb(161, 240, 220)'
-									: 'rgb(240, 161, 168)'
-							}
-						/>
-					);
-				})}
+				{!transactions ? (
+					<PreLoader />
+				) : !filterText ? (
+					<ListTransaction
+						transactions={transactions}
+						handleMessageUser={handleMessageUser}
+					/>
+				) : (
+					<ListTransaction
+						transactions={filterText}
+						handleMessageUser={handleMessageUser}
+					/>
+				)}
+
 				<NewTransactionModal
 					modalIsOpen={modalIsOpen}
 					closeModal={closeModal}
